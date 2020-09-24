00d47c8d436ae3a7504aa9941a8001b73d4b8ae1
Marcelo Cohen <mflashbr@gmail.com>
2020-09-24 09:21:31 -0300

    Movimento com entradas nomeadas e clique com o mouse

---
+++ Assets/MouseTo3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTo3D : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
+        RaycastHit rayCastInfo;
+        if(Physics.Raycast(ray, out rayCastInfo)) {
+            GameObject obj = rayCastInfo.transform.gameObject;
+            print(obj.name + " - " + rayCastInfo.point);
+            if(obj.tag == "cubos" && Input.GetMouseButton(0)) {
+                Renderer r = obj.GetComponent<Renderer>();
+                r.material.color = Color.gray;
+            }
+        }
+    }
+}
--- Assets/Scripts/MoveVehicle.cs
+++ Assets/Scripts/MoveVehicle.cs
@@ -16,14 +16,16 @@ public class MoveVehicle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        else if(Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        float speedY = moveSpeed * Input.GetAxis("Vertical");
+        // if(Input.GetKey(KeyCode.UpArrow))
+        transform.Translate(Vector3.forward * speedY * Time.deltaTime);
+        // else if(Input.GetKey(KeyCode.DownArrow))
+            // transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
 
-        if(Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-        else if(Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.up,  turnSpeed * Time.deltaTime);
+        float turnSpeedAdj = turnSpeed * Input.GetAxis("Horizontal");
+        // if(Input.GetKey(KeyCode.LeftArrow))
+        transform.Rotate(Vector3.up, turnSpeedAdj * Time.deltaTime);
+        // else if(Input.GetKey(KeyCode.RightArrow))
+            // transform.Rotate(Vector3.up,  turnSpeed * Time.deltaTime);
     }
 }
