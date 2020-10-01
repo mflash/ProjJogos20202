c70bcf0fe806c927949b3f6d2ea490a6b2fdb524
Marcelo Cohen <mflashbr@gmail.com>
2020-10-01 09:57:15 -0300

    Sprites e animações, eventos de animação (veja código de WalkAndRun)

---
+++ Assets/Scripts/WalkAndRun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkAndRun : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+            anim.SetBool("IsRunning", true);
+        else
+            anim.SetBool("IsRunning", false);
+    }
+
+    public void PrintEvent(float value)
+    {
+        Debug.Log("PrintEvent: " + value);
+    }
+}
