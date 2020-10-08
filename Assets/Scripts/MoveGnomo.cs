87362317b0db5c5091fa5434e72c9926f875f191
Marcelo Cohen <mflashbr@gmail.com>
2020-10-08 09:31:16 -0300

    Movimento com Física

---
+++ Assets/Scripts/MoveGnomo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGnomo : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = Input.GetAxis("Horizontal") * speed;
+        //Vector2 vel = new Vector2(delta,rb.velocity.y);
+        //rb.velocity = vel;
+        Vector2 forca = new Vector2(delta,0);
+        rb.AddForce(forca, ForceMode2D.Impulse);
+    }
+}
